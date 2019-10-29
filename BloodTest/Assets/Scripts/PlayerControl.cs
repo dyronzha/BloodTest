6a5bc38d13d7ec754c21a26172582011129e29e8
YeeLong <dyron85511@gmail.com>
2019-10-29 22:42:59 +0800

    blend tree

---
+++ BloodTest/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerControl : MonoBehaviour
+{
+    float inputMoveX, inputMoveY;
+    Player playerInput;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        playerInput = ReInput.players.GetPlayer(0);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputMoveX = playerInput.GetAxis("Move Horizontal");
+        inputMoveY = playerInput.GetAxis("Move Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+
+
+}
