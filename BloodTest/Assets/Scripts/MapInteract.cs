09c3ede8319b08a9e9037ead1ad57413ecff302c
dyronzha <dyron85511@gmail.com>
2019-11-18 02:56:37 +0800

    interact cumston editor

---
+++ BloodTest/Assets/Scripts/MapInteract.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapInteract : MonoBehaviour
+{
+    [HideInInspector]
+    public Drink[] Drinks = new Drink[0];
+
+    [System.Serializable]
+    public struct Drink
+    {
+        public string Name;
+        public float Price;
+        public Color Color;
+        public int aaa;
+    }
+
+    [HideInInspector]
+    public VCameraPoint[] VCameraPoints = new VCameraPoint[0];
+
+    [System.Serializable]
+    public struct VCameraPoint {
+        public Collider colliderPoint;
+        public GameObject vCamera;
+    }
+
+
+    [HideInInspector]
+    public interactPoint[] interactPoints = new interactPoint[0];
+
+    [System.Serializable]
+    public struct interactPoint
+    {
+        public Collider colliderPoint;
+        public InteractType interactType;
+        public UnityEngine.Playables.PlayableDirector timeline;
+        public float textShowTime;
+        public string infoText;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
+
+public enum InteractType
+{
+    CameraMove, InfoText, MoveAndText
+}
